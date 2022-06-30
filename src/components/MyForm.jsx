cdb1663798b0d686e77775fcfc3b865ac617daed
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-06-29 20:51:35 -0400

    Implement code for successful form registration by adding conditionals to fill inputs and thank you note

--- src/components/MyForm.jsx
+++ src/components/MyForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./MyForm.css";
 
 export const MyForm = () => {
   const [userInfo, setUserInfo] = useState({
@@ -6,33 +7,59 @@ export const MyForm = () => {
     lastName: "",
     email: "",
   });
+  const [warning, setWarning] = useState(false);
+  const [validSubmission, setValidSubmission] = useState(false);
 
   const handleChange = (event) => {
     const { name, value } = event.target;
     setUserInfo({ ...userInfo, [name]: value });
   };
 
+  const handleRegister = (event) => {
+    event.preventDefault();
+    if (userInfo.firstName && userInfo.lastName && userInfo.email) {
+      setValidSubmission(true);
+    }
+    setWarning(true);
+  };
+
   return (
-    <div className="form">
-      <input
-        placeholder="Write your first name"
-        name="firstName"
-        value={userInfo.firstName}
-        onChange={handleChange}
-      />
-      <input
-        placeholder="Write your last name"
-        name="lastName"
-        value={userInfo.lastName}
-        onChange={handleChange}
-      />
-      <input
-        placeholder="Write your email"
-        name="email"
-        value={userInfo.email}
-        onChange={handleChange}
-      />
-      <button>Sign Up</button>
-    </div>
+    <form className="form" onSubmit={handleRegister}>
+      <div className="title">Registration Form</div>
+      {warning && validSubmission ? (
+        <div className="thank-you">Thank you for registering!</div>
+      ) : (
+        <>
+          <input
+            placeholder="First name"
+            name="firstName"
+            value={userInfo.firstName}
+            onChange={handleChange}
+          />
+          {warning && !userInfo.firstName ? (
+            <span>Please enter your first name!</span>
+          ) : null}
+          <input
+            placeholder="Last name"
+            name="lastName"
+            value={userInfo.lastName}
+            onChange={handleChange}
+          />
+          {warning && !userInfo.lastName ? (
+            <span>Please enter your last name!</span>
+          ) : null}
+          <input
+            placeholder="Email"
+            name="email"
+            value={userInfo.email}
+            onChange={handleChange}
+          />
+          {warning && !userInfo.email ? (
+            <span>Please enter your email!</span>
+          ) : null}
+          <button type="submit">Sign Up</button>
+        </>
+      )}
+    </form>
   );
 };
